dcfdf2508911fb12b08954235140c5d95b4bc4aa
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-02-28 18:13:11 +0600

    Update views.py

--- My_Second_Project/Login_app/views.py
+++ My_Second_Project/Login_app/views.py
@@ -7,8 +7,6 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.contrib.auth.models import User
-# Create your views here.
-
 
 
 
